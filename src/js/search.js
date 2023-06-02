52aa3b924f932f2b1f886d4383f336a19ef61aeb
Thiago <thiagoalgusto_2025@ghotmail.com.br>
2023-06-02 11:25:37 -0300

    pesquisa de item

--- src/js/search.js
+++ src/js/search.js
@@ -1,1 +1,11 @@
-import '../assets/css/search.css';
+// import '../assets/css/search.css';
+
+let animals = [{type: "lion" }, "tiger"];
+let clones = animals.slice();
+
+
+clones[0].type = "bear";
+clones[1] = 'sheep';
+
+console.log(animals[0].type, clones[0].type);
+console.log(animals[1], clones[1]);
